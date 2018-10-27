67f8e57bc6d6d71f134b781c1a3786a62a12ab3a
jennifer <teriyakibuns@yahoo.com>
2018-10-27 13:59:34 -0700

    make new card with cancel and done buttons

--- Flashcards-codepath/Flashcards/ViewController.swift
+++ Flashcards-codepath/Flashcards/ViewController.swift
@@ -10,69 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var backLabel: UILabel!
-    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel! // answer
+    @IBOutlet weak var frontLabel: UILabel! // question
     @IBOutlet weak var card: UIView!
+    @IBOutlet weak var correctLabel: UILabel!
     
     @IBOutlet weak var btnOptionOne: UIButton!
     @IBOutlet weak var btnOptionTwo: UIButton!
     @IBOutlet weak var btnOptionThree: UIButton!
     
+    @IBAction func makeBtnPretty(_ sender: UIButton) {
+        sender.layer.borderWidth = 3.0
+        sender.layer.borderColor = #colorLiteral(red: 0.9666306376, green: 0.590148747, blue: 0.7345550656, alpha: 1)
+        sender.layer.cornerRadius = 20.0
+        sender.clipsToBounds = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        card.layer.cornerRadius = 20.0; // add round corners to view
-        frontLabel.layer.cornerRadius = 20.0; // add round corners to question
-        backLabel.layer.cornerRadius = 20.0; // add round corners to answer
         
-        frontLabel.clipsToBounds = true; // show round corners on questions
-        backLabel.clipsToBounds = true; // show round corners on answer
+        frontLabel.layer.cornerRadius = 20.0 // add round corners to question
+        frontLabel.clipsToBounds = true // show round corners on questions
         
-        card.layer.shadowRadius = 15.0; // add shadow
-        card.layer.shadowOpacity = 0.2; // change shadow opacity
+        backLabel.layer.cornerRadius = 20.0 // add round corners to answer
+        backLabel.clipsToBounds = true // show round corners on answer
         
-        btnOptionOne.layer.borderWidth = 3.0;
-        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.9666306376, green: 0.590148747, blue: 0.7345550656, alpha: 1);
-        btnOptionOne.layer.cornerRadius = 20.0;
-        btnOptionOne.clipsToBounds = true;
+        card.layer.cornerRadius = 20.0 // add round corners to view
+        card.layer.shadowRadius = 15.0 // add shadow
+        card.layer.shadowOpacity = 0.2 // change shadow opacity
         
-        btnOptionTwo.layer.borderWidth = 3.0;
-        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.9666306376, green: 0.590148747, blue: 0.7345550656, alpha: 1);
-        btnOptionTwo.layer.cornerRadius = 20.0;
-        btnOptionTwo.clipsToBounds = true;
+        self.makeBtnPretty(btnOptionOne)
+        self.makeBtnPretty(btnOptionTwo)
+        self.makeBtnPretty(btnOptionThree)
         
-        btnOptionThree.layer.borderWidth = 3.0;
-        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.9666306376, green: 0.590148747, blue: 0.7345550656, alpha: 1);
-        btnOptionThree.layer.cornerRadius = 20.0;
-        btnOptionThree.clipsToBounds = true;
+        correctLabel.isHidden = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // destination of the segue is the navigation controller
+        let navigationController = segue.destination as! UINavigationController
+        // navigation controller only contains a view controller
+        let creationController = navigationController.topViewController as! CreationViewController
+        // set flashcards controller property to self
+        creationController.flashcardController = self
+        
     }
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if (frontLabel.isHidden){ // answer is showing
             // i.e. user wants to see the questions and all the choices again
             // reset everything
-            btnOptionOne.isHidden = false;
-            btnOptionTwo.isHidden = false;
-            btnOptionThree.isHidden = false;
-            frontLabel.isHidden = false; // show question
+            btnOptionOne.isHidden = false
+            btnOptionTwo.isHidden = false
+            btnOptionThree.isHidden = false
+            frontLabel.isHidden = false // show question
         } else { // if question is showing
             // i.e. user gave up they want to see answer
-            self.didTapOptionOne(sender);
+            self.didTapOptionOne(sender)
         }
+        correctLabel.isHidden = true
+    }
+    
+    func updateFlashcard(question: String, answer: String) {
+        frontLabel.text = question
+        backLabel.text = answer
     }
     
     @IBAction func didTapOptionOne(_ sender: Any) {
-        frontLabel.isHidden = true; // show answer
-        btnOptionOne.isHidden = true; // hide all buttons
-        btnOptionTwo.isHidden = true;
-        btnOptionThree.isHidden = true;
+        frontLabel.isHidden = true // show answer
+        btnOptionOne.isHidden = true // hide all buttons
+        btnOptionTwo.isHidden = true
+        btnOptionThree.isHidden = true
+        correctLabel.isHidden = false
     }
     
     @IBAction func didTapOptionTwo(_ sender: Any) {
-        btnOptionTwo.isHidden = true;
+        btnOptionTwo.isHidden = true
     }
     
     @IBAction func didTapOptionThree(_ sender: Any) {
-        btnOptionThree.isHidden = true;
+        btnOptionThree.isHidden = true
     }
 }
