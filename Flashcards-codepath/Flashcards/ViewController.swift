07057e2d595ebfd6ef78d9f8d71b1782306e4b1d
jennifer <teriyakibuns@yahoo.com>
2018-11-06 00:39:39 -0800

    delete button working but read and write to disk is not

--- Flashcards-codepath/Flashcards/ViewController.swift
+++ Flashcards-codepath/Flashcards/ViewController.swift
@@ -35,11 +35,11 @@ class ViewController: UIViewController {
     // current flashcard index
     var currentCard = 0
     
-    @IBAction func makeBtnPretty(_ sender: UIButton) {
-        sender.layer.borderWidth = 3.0
-        sender.layer.borderColor = #colorLiteral(red: 0.9666306376, green: 0.590148747, blue: 0.7345550656, alpha: 1)
-        sender.layer.cornerRadius = 20.0
-        sender.clipsToBounds = true
+    @IBAction func makeBtnPretty(btn: UIButton) {
+        btn.layer.borderWidth = 3.0
+        btn.layer.borderColor = #colorLiteral(red: 0.9666306376, green: 0.590148747, blue: 0.7345550656, alpha: 1)
+        btn.layer.cornerRadius = 20.0
+        btn.clipsToBounds = true
     }
     
     override func viewDidLoad() {
@@ -56,9 +56,9 @@ class ViewController: UIViewController {
         card.layer.shadowRadius = 15.0 // add shadow
         card.layer.shadowOpacity = 0.2 // change shadow opacity
         
-        self.makeBtnPretty(btnOptionOne)
-        self.makeBtnPretty(btnOptionTwo)
-        self.makeBtnPretty(btnOptionThree)
+        self.makeBtnPretty(btn: btnOptionOne)
+        self.makeBtnPretty(btn: btnOptionTwo)
+        self.makeBtnPretty(btn: btnOptionThree)
         
         correctLabel.isHidden = true
         
@@ -71,7 +71,15 @@ class ViewController: UIViewController {
         backLabel.isUserInteractionEnabled = true
         backLabel.addGestureRecognizer(tapBack)
         
-        updateFlashcard(updatedQuestion: "where in the world is carmen", updatedAnswer: "san diego", updatedAnswer2: "san francisco", updatedAnswer3: "santa monica", addNewCard: true)
+        // read saved flashcards from disk if any
+        //        readSavedFlashcards()
+        
+        if flashcardArr.count == 0 {
+            updateFlashcard(updatedQuestion: "where in the world is carmen", updatedAnswer: "san diego", updatedAnswer2: "san francisco", updatedAnswer3: "santa monica", addNewCard: true)
+        }else{
+            updateLabels()
+            updateNextAndPrevButtons()
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -79,13 +87,10 @@ class ViewController: UIViewController {
         let navigationController = segue.destination as! UINavigationController
         
         // What to do with the editing page -> should it be populated with data or blank?
-        
-        
-        if segue.identifier != "SettingSegue"{
+        if segue.identifier != "SettingSegue" {
             // navigation controller only contains a view controller
             let creationController = navigationController.topViewController as! CreationViewController
             creationController.flashcardController = self
-            
             
             // separate different segues with different actions
             // user wants to edit, page must be pre-populated with data
@@ -95,14 +100,9 @@ class ViewController: UIViewController {
                 
                 creationController.extraAns2 = btnOptionTwo.titleLabel!.text
                 creationController.extraAns3 = btnOptionThree.titleLabel!.text
-                
-                creationController.addNew = false
             }
-            
-            // user wants to add a new card, do not pre-populate page with data
-            if segue.identifier == "PlusSegue" {
-                creationController.addNew = true
-            }
+            // determine whether user is editing or adding a card
+            creationController.addNew = segue.identifier == "PlusSegue" ? true : false
         }
         
         // user tapped settings, send user to settings page
@@ -111,13 +111,13 @@ class ViewController: UIViewController {
         }
     }
     
-    @objc func didTapFrontLabel(_ sender: Any){
+    @objc func didTapFrontLabel(){
         // user gave up, show answer
-        self.didTapOptionOne(sender)
+        self.didTapOptionOne()
         correctLabel.isHidden = true
     }
     
-    @objc func didTapBackLabel(_ sender: Any){
+    @objc func didTapBackLabel(){
         // answer is showing, user wants to see the questions and all the choices again, reset everything
         btnOptionOne.isHidden = false
         btnOptionTwo.isHidden = false
@@ -134,6 +134,11 @@ class ViewController: UIViewController {
         btnOptionOne.setTitle(flashcardArr[currentCard].answer, for: .normal)
         btnOptionTwo.setTitle(flashcardArr[currentCard].answer2, for: .normal)
         btnOptionThree.setTitle(flashcardArr[currentCard].answer3, for: .normal)
+        
+        // make sure the next and prev buttons are properly disabled
+        updateNextAndPrevButtons()
+        
+        didTapBackLabel() // reset everything
     }
     
     func updateFlashcard(updatedQuestion: String, updatedAnswer: String, updatedAnswer2: String, updatedAnswer3: String, addNewCard: Bool) {
@@ -147,23 +152,19 @@ class ViewController: UIViewController {
             
             // update current index
             currentCard = flashcardArr.count - 1
-            
-            // update the labels
-            updateLabels()
-            
-            // make sure the next and prev buttons are properly disabled
-            updateNextAndPrevButtons()
         }else{
             // update the current index with the edited flashcard object
             flashcardArr[currentCard] = flashcard
-            
-            // update labels
-            updateLabels()
         }
+        // update labels
+        updateLabels()
+        
+        //        saveAllFlashcardsToDisk()
+        
         print("total cards: \(flashcardArr.count) all flashcards: \(flashcardArr)")
     }
     
-    @IBAction func didTapOptionOne(_ sender: Any) {
+    @IBAction func didTapOptionOne() {
         frontLabel.isHidden = true // show answer
         btnOptionOne.isHidden = true // hide all buttons
         btnOptionTwo.isHidden = true
@@ -171,11 +172,11 @@ class ViewController: UIViewController {
         correctLabel.isHidden = false
     }
     
-    @IBAction func didTapOptionTwo(_ sender: Any) {
+    @IBAction func didTapOptionTwo() {
         btnOptionTwo.isHidden = true
     }
     
-    @IBAction func didTapOptionThree(_ sender: Any) {
+    @IBAction func didTapOptionThree() {
         btnOptionThree.isHidden = true
     }
     
@@ -185,15 +186,81 @@ class ViewController: UIViewController {
         btnPrev.isEnabled = currentCard == 0 ? false : true
     }
     
-    @IBAction func didTapPrev(_ sender: Any) {
+    @IBAction func didTapPrev() {
         currentCard -= 1
-        updateLabels()
-        updateNextAndPrevButtons()
+        updateLabels() // reset everything
     }
     
-    @IBAction func didTapNext(_ sender: Any) {
+    @IBAction func didTapNext() {
         currentCard += 1
         updateLabels()
-        updateNextAndPrevButtons()
+    }
+    
+    @IBAction func swipeAction(_ gestureRecognizer: UISwipeGestureRecognizer) {
+        print("swiped! \(gestureRecognizer.direction)")
+        if gestureRecognizer.direction.rawValue == 1 && currentCard != 0 {
+            print("swiped right")
+            didTapPrev()
+        }
+        // if gestureRecognizer.direction.rawValue == 2 && currentCard != flashcardArr.count - 1 { print("swiped left") }
+    }
+    
+    func saveAllFlashcardsToDisk() {
+        // save array to disk using user defaults
+        // "set" means array and it must have a unique string key
+        // however user defaults only reads array of dictionaries, must convert to dictionary
+        let dictionaryArr = flashcardArr.map { (card) -> [String: String] in
+            return ["question": card.question, "answer":card.answer, "answer2":card.answer2, "answer3":card.answer3]
+        }
+        print("dictionary: \(dictionaryArr)")
+        UserDefaults.standard.set(flashcardArr, forKey: "flashcardArr")
+    }
+    
+    func readSavedFlashcards(){
+        // read dictionary array from disk if any, use dictionary's unique key we gave earlier
+        // "if let" will store a value in the variable if it exists
+        if let dictionaryArr = UserDefaults.standard.array(forKey: "flashcardArr") as? [[String: String]]{
+            // convert the dictionary back to an array
+            let savedCards = dictionaryArr.map{ dictionary -> Flashcard in
+                return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!, answer2: dictionary["answer2"]!, answer3: dictionary["answer3"]!)
+            }
+            // add all the cards to the array
+            flashcardArr.append(contentsOf: savedCards)
+        }
+    }
+    
+    @IBAction func didTapDelete(_ sender: Any) {
+        // confirm deletion
+        let alert = UIAlertController(title: "Delete Flashcard", message: "Are you sure you want to delete this flashcard?", preferredStyle: .actionSheet)
+        // destructive makes the GUI look red and bold
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+            self.deleteCurrentCard()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    func deleteCurrentCard(){
+        // do not remove the last card in the deck
+        if flashcardArr.count == 1 {
+            let alert = UIAlertController(title: "Cannot Delete", message: "You can't delete this card because the deck would be empty.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(alert, animated: true)
+        }else{
+            // proceed with deletion
+            // removes item at current index, all items behind it get pushed forward
+            flashcardArr.remove(at: currentCard)
+            
+            // move current index if deleting last card
+            if currentCard > flashcardArr.count - 1 {
+                currentCard = flashcardArr.count - 1
+            }
+            
+            updateLabels()
+            //        saveAllFlashcardsToDisk()
+        }
     }
 }
